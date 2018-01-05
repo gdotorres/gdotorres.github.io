99d2390984e75b1ed55190d84b5c3a269286f40a
Giovane <gdotorres@inf.ufpel.edu.br>
2018-01-05 21:18:16 -0200

    Inserting ovg.html

---
+++ scripts/btns.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("#btn_rapha").click(function(){
+		$("p#story").html("<b>Raphael Nickel</b>, também conhecido como <b>Rapha</b>, <b>Big Richard</b>, <b>Thor the Powerhead</b> e <b>Cygnus Cutallion</b> (escrevi essa porra certa será?) é o <strike>ditador tirano</strike> administrador do grupo. O apelido Big Richard vem da grande arte da conquista que o meliante tem com mulheres casadas, sendo referência na área. Além disso, é um moleque safado que já deve ter passado a rola em quase toda a população da Grande Porto Alegre. Se não passou a rola, já passou os lábios. Se não passou os lábios, passou a mão. Também tem como características marcantes fazer eventuais piadas de tiozão e rir de qualquer bobagem que é dita na internet. Ele também é um assíduo tibiano, e é patrocinado pela Heineken")
+	});
+	$("#btn_vini").click(function(){
+		$("p#story").html("<b>Vinícius Alves</b>, também conhecido como <b>Vini</b>, <b>Vinicio</b>, <b>Prejereba</b>, <b>Jeferson</b> e <b>Saint Catarine</b> pode ser considerado como o <strike>vice-ditador tirano</strike> segundo-administrador do grupo, quando da ausência de <b>Raphael</b>. Vinicio é um jovem jogador de Tibia, o qual deve gastar 70% do dia jogando este jogo da época dos dinossauros. Vini é membro do grupo de <b>super-virgens</b>, pessoas estas que escolheram esperar. É o membro do grupo com maior quantidade de mensagens por dia, mostrando que ele tem muitas outras coisas interessantes pra fazer. É outro meliante que domina a difícil técnica de <i>Big Uncle Jokes</i>")
+	});
+	$("#btn_jovane").click(function(){
+		$("p#story").html("<b>Giovane Torres</b>, também conhecido como <b>Jovane</b>, (eu tenho mais apelidos? tenho não!) é o medalhista de bronze em perder tempo, já que é o terceiro membro com mais mensagens no grupo. Tem um senso de humor doentio e retardado, rindo de qualquer bosta que mandarem pra ele. Odeia <i>normies</i> com toda a força do mundo, e é um sem-paciência na arte da conquista, visto que usa de meios ilícitos para prazeres carnais. Tem mestrado em piadas de tiozão.")
+	});
+	$("#btn_guto").click(function(){
+		$("p#story").html("<b>Augusto Rodrigues</b>, também conhecido como <b>Guto</b> ou <b>Augustinho</b>, ou ainda <b>Augustinho Carrara</b>, e também <b>Jesus</b> (dada a semelhança física) é o membro do grupo que precisa do PROERD imediatamente <strike>zoas, pode fumar teus bagulho aí</strike>. Tem doutorado na arte da conquista de meninas alternativas. Mora na mesma cidade que Jovane. Provavelmente é o membro com maior pança do grupo. (Aliás quantos quilos tu pesa, Augustinho? Fica minha dúvida)")
+	});
+});
